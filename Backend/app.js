67d47adbe2859da042ec2219023130c3960fce24
dinith-rusiru <it22145284@my.sliit.lk>
2025-03-17 17:10:46 +0530

    up

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose= require("mongoose");
-//const router = require("./Routes/GshoopperRoutes")
-const router = require("./Routes/ListManagerRoutes")
+const router = require("./Routes/GshoopperRoutes")
+const fmemberRoutes = require("../Backend/Routes/FmemberRoutes");
 
 const app = express();
 const cors = require("cors");
@@ -9,18 +9,14 @@ const cors = require("cors");
 //Middleware 
 app.use(express.json());
 app.use(cors());
+app.use("/gshoppers",router);
+// CHANGE: Using the same router for usages instead of creating a new one
+// app.use("/usages", router); // Same route handler for usage records
+app.use("/fmembers", fmemberRoutes);
 
-
-app.use("/lists",router);
-
-//mongoose.connect ("mongodb+srv://admin:le7161C9pwmC89qo@cluster0.fpzv9.mongodb.net/")
-mongoose.connect ("mongodb+srv://admin:NieTuCwZPlz2Av6I@cluster0.r2x9s.mongodb.net/")
+mongoose.connect ("mongodb+srv://admin:le7161C9pwmC89qo@cluster0.fpzv9.mongodb.net/")
 .then(()=>console.log("Connected to mongodb"))
 .then(()=>{
     app.listen(5000);
 })
 .catch((err)=>console.log((err)));
-
-
-
-//NieTuCwZPlz2Av6I mongodb password
