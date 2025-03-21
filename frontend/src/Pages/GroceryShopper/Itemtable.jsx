8185a8298094fd4f5bcfca8383c80257b9f81008
IT22099686 <127779990+Nadun25@users.noreply.github.com>
2025-03-21 18:46:51 +0530

    .

--- frontend/src/Pages/GroceryShopper/Itemtable.jsx
+++ frontend/src/Pages/GroceryShopper/Itemtable.jsx
@@ -1082,7 +1082,7 @@ function Itemtable() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
               </svg>
             </button>
-            <button
+            {/* <button
               className="p-1.5 rounded-lg text-gray-600 hover:bg-gray-100 transition-colors"
               title="View Details"
               onClick={() => navigate(`/itemdetails/${_id}`)}
@@ -1090,7 +1090,7 @@ function Itemtable() {
               <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
-            </button>
+            </button> */}
           </div>
         </td>
       </tr>
